3a973fd1b5aa8e5e92a70a8543642749d9418756
szukacs <szukacs.gergely@gmail.com>
2020-09-23 22:24:18 +0200

    GB-16 Get students endpoint with filtered by suspicion

--- src/main/java/com/example/fedex_backend/services/student/StudentService.java
+++ src/main/java/com/example/fedex_backend/services/student/StudentService.java
@@ -2,8 +2,12 @@ package com.example.fedex_backend.services.student;
 
 import com.example.fedex_backend.models.student.Student;
 
+import java.util.List;
+
 public interface StudentService {
     Student addStudent(Student student);
 
     Student saveStudent(Student student);
+
+    List<Student> getAllStudentFilteredBySuspicion();
 }
