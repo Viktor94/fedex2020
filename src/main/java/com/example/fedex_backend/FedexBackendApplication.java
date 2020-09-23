f92861456ca8cb5dde7771d302da96be09a671f5
Viktor94 <viktor2.nagy@gmail.com>
2020-09-23 10:18:28 +0200

    Project setup

---
+++ src/main/java/com/example/fedex_backend/FedexBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.fedex_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FedexBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(FedexBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/example/fedex_backend/FedexBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fedex_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FedexBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
