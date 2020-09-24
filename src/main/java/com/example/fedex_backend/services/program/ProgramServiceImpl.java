5c9c6b0b6a97a77f4310204b69d80aea4fd8a3b1
szukacs <szukacs.gergely@gmail.com>
2020-09-24 22:01:54 +0200

    bugfixProgram

--- src/main/java/com/example/fedex_backend/services/program/ProgramServiceImpl.java
+++ src/main/java/com/example/fedex_backend/services/program/ProgramServiceImpl.java
@@ -12,85 +12,94 @@ import com.example.fedex_backend.models.student.Student;
 import com.example.fedex_backend.repositories.ProgramCpuUsageRepository;
 import com.example.fedex_backend.repositories.ProgramMemoryUsageRepository;
 import com.example.fedex_backend.repositories.ProgramRepository;
-import java.util.ArrayList;
-import java.util.List;
 import org.slf4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Service
 public class ProgramServiceImpl implements ProgramService {
 
-  private final ProgramRepository programRepository;
-  private final Logger LOGGER;
-  private final ProgramCpuUsageRepository programCpuUsageRepository;
-  private final ProgramMemoryUsageRepository programMemoryUsageRepository;
+    private final ProgramRepository programRepository;
+    private final Logger LOGGER;
+    private final ProgramCpuUsageRepository programCpuUsageRepository;
+    private final ProgramMemoryUsageRepository programMemoryUsageRepository;
 
-  @Autowired
-  public ProgramServiceImpl(
-      ProgramRepository programRepository,
-      Logger logger,
-      ProgramCpuUsageRepository programCpuUsageRepository,
-      ProgramMemoryUsageRepository programMemoryUsageRepository) {
-    this.programRepository = programRepository;
-    this.LOGGER = logger;
-    this.programCpuUsageRepository = programCpuUsageRepository;
-    this.programMemoryUsageRepository = programMemoryUsageRepository;
-  }
-
-  @Override
-  public List<Program> getAllProgram() {
-    return (List<Program>) programRepository.findAll();
-  }
-
-  @Override
-  public ArrayList<Program> savePrograms(Student student, List<ProgramDTO> programDTOlist) {
-    ArrayList<Program> programList = new ArrayList<>();
-    for (ProgramDTO programDTO : programDTOlist) {
-      Program program = new Program(programDTO);
-      if (programRepository.findByProgramName(program.getProgramName()).isEmpty()) {
-        program.addStudent(student);
-        programList.add(program);
-      }
+    @Autowired
+    public ProgramServiceImpl(
+            ProgramRepository programRepository,
+            Logger logger,
+            ProgramCpuUsageRepository programCpuUsageRepository,
+            ProgramMemoryUsageRepository programMemoryUsageRepository) {
+        this.programRepository = programRepository;
+        this.LOGGER = logger;
+        this.programCpuUsageRepository = programCpuUsageRepository;
+        this.programMemoryUsageRepository = programMemoryUsageRepository;
     }
-    programRepository.saveAll(programList);
-    return programList;
-  }
 
-  @Override
-  public void updateProgramSuspicion(Long id, ProgramUpdateDTO programUpdateDTO)
-      throws ProgramException {
-    Program program = programRepository.findById(id).orElse(null);
-    if (program != null) {
-      program.setIsAllowed(programUpdateDTO.getIsAllowed());
-      programRepository.save(program);
-    } else {
-      LOGGER.info("User not found with the provided id: " + id);
-      throw new ProgramNotFoundByIdException("User not found with the provided id: " + id);
+    @Override
+    public List<Program> getAllProgram() {
+        return (List<Program>) programRepository.findAll();
     }
-  }
 
-  @Override
-  public void saveProgramMemoryUsage(Student student, List<ProgramDTO> programDTOlist) {
-    programMemoryUsageRepository.deleteAll(student.getProgramMemoryUsages());
-    ArrayList<ProgramMemoryUsage> programMemoryUsageList = new ArrayList<>();
-    for (ProgramDTO programDTO : programDTOlist) {
-      ProgramMemoryUsage programMemoryUsage = new ProgramMemoryUsage(programDTO);
-      programMemoryUsage.setStudent(student);
-      programMemoryUsageList.add(programMemoryUsage);
+    @Override
+    public ArrayList<Program> savePrograms(Student student, List<ProgramDTO> programDTOlist) {
+        ArrayList<Program> programList = new ArrayList<>();
+        for (ProgramDTO programDTO : programDTOlist) {
+            Optional<Program> optionalProgram = programRepository.findByProgramName(programDTO.getName());
+            if (optionalProgram.isEmpty()) {
+                Program program = new Program(programDTO);
+                program.addStudent(student);
+                programList.add(program);
+            } else {
+                Program existingProgram = optionalProgram.get();
+                if (!existingProgram.getStudentList().contains(student)) {
+                    existingProgram.addStudent(student);
+                    programList.add(existingProgram);
+                }
+            }
+        }
+        programRepository.saveAll(programList);
+        return programList;
     }
-    programMemoryUsageRepository.saveAll(programMemoryUsageList);
-  }
 
-  @Override
-  public void saveProgramCpuUsage(Student student, List<CpuDTO> cpuDTOList) {
-    programCpuUsageRepository.deleteAll(student.getProgramCpuUsages());
-    ArrayList<ProgramCpuUsage> programMemoryUsageList = new ArrayList<>();
-    for (CpuDTO cpuDTO : cpuDTOList) {
-      ProgramCpuUsage programCpuUsage = new ProgramCpuUsage(cpuDTO);
-      programCpuUsage.setStudent(student);
-      programMemoryUsageList.add(programCpuUsage);
+    @Override
+    public void updateProgramSuspicion(Long id, ProgramUpdateDTO programUpdateDTO)
+            throws ProgramException {
+        Program program = programRepository.findById(id).orElse(null);
+        if (program != null) {
+            program.setIsAllowed(programUpdateDTO.getIsAllowed());
+            programRepository.save(program);
+        } else {
+            LOGGER.info("User not found with the provided id: " + id);
+            throw new ProgramNotFoundByIdException("User not found with the provided id: " + id);
+        }
     }
-    programCpuUsageRepository.saveAll(programMemoryUsageList);
-  }
+
+    @Override
+    public void saveProgramMemoryUsage(Student student, List<ProgramDTO> programDTOlist) {
+        programMemoryUsageRepository.deleteAll(student.getProgramMemoryUsages());
+        ArrayList<ProgramMemoryUsage> programMemoryUsageList = new ArrayList<>();
+        for (ProgramDTO programDTO : programDTOlist) {
+            ProgramMemoryUsage programMemoryUsage = new ProgramMemoryUsage(programDTO);
+            programMemoryUsage.setStudent(student);
+            programMemoryUsageList.add(programMemoryUsage);
+        }
+        programMemoryUsageRepository.saveAll(programMemoryUsageList);
+    }
+
+    @Override
+    public void saveProgramCpuUsage(Student student, List<CpuDTO> cpuDTOList) {
+        programCpuUsageRepository.deleteAll(student.getProgramCpuUsages());
+        ArrayList<ProgramCpuUsage> programMemoryUsageList = new ArrayList<>();
+        for (CpuDTO cpuDTO : cpuDTOList) {
+            ProgramCpuUsage programCpuUsage = new ProgramCpuUsage(cpuDTO);
+            programCpuUsage.setStudent(student);
+            programMemoryUsageList.add(programCpuUsage);
+        }
+        programCpuUsageRepository.saveAll(programMemoryUsageList);
+    }
 }
